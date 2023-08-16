c716ac385ba06f5166dfa999b66a2db37de9f785
rettranksX <rettranks@gmail.com>
2023-08-16 16:58:33 +0300

    Test

--- app/code/Dev/RestApi/Model/Api/ProductRepository.php
+++ app/code/Dev/RestApi/Model/Api/ProductRepository.php
@@ -233,7 +233,7 @@ class ProductRepository implements ProductRepositoryInterface
 
                 $countryModel = $this->countryFactory->create();
                 $countryCollection = $countryModel->getCollection();
-                $country = $countryCollection->addFieldToFilter('name', $countryName)->getFirstItem();
+                $country = $countryCollection->addFieldToFilter('full_name_locale', $countryName)->getFirstItem();
                 
                 if ($country->getId()) {
                     $isoCountryCode = $country->getIso2Code();
