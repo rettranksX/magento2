5ab0f1f6f9e0dffad5468dcc3911c809952c0a8d
rettranksX <rettranks@gmail.com>
2023-08-22 15:36:25 +0300

    test

--- app/code/Dev/RestApi/Model/Api/ProductRepository.php
+++ app/code/Dev/RestApi/Model/Api/ProductRepository.php
@@ -320,6 +320,78 @@ class ProductRepository implements ProductRepositoryInterface
                     }
                 }
             }
+            if ($details == 1) {
+                foreach ($productCollection as $product) {
+                    if (in_array($product->getSku(), $skuArray)) {
+                        $deliveryOptions = [];
+
+                        $productImage = $this->_productRepositoryFactory->create()->getById($product->getId());
+                        $image = $productImage->getData('image');
+                        $thumbnail = $productImage->getData('thumbnail');
+                        $smallImage = $productImage->getData('small_image');
+                        $images = [$image, $thumbnail, $smallImage];
+
+                        $countryName = $product->getAttributeText('country_of_manufacture');
+
+                        $countryModel = $this->countryFactory->create();
+                        $countryCollection = $countryModel->getCollection();
+                        $country = $countryCollection->addFieldToFilter('iso2_code', $countryName)->getFirstItem();
+
+                        if ($country->getId()) {
+                            $isoCountryCode = $country->getIso2Code();
+                        } else {
+                            $isoCountryCode = $countryName;
+                        }
+
+                        $availableMethods = [];
+                        $carriers = $this->shippingConfig->getActiveCarriers();
+
+                        foreach ($carriers as $carrierCode => $carrierModel) {
+                            $pathPrice = "carriers/{$carrierCode}/price";
+                            $pathEstimateTime = "carriers/{$carrierCode}/estimated_delivery_time";
+                            $storeScope = \Magento\Store\Model\ScopeInterface::SCOPE_STORE;
+                            $availableMethods[] = [
+                                'name' => $carrierCode,
+                                'shippingRate' => $this->scopeConfig->getValue($pathPrice, $storeScope),
+                                'deliveryDays' => $this->scopeConfig->getValue($pathEstimateTime, $storeScope),
+                            ];
+                        }
+
+                        $deliveryOptions[] = [
+                            "country" => $isoCountryCode,
+                            "carriers" => $availableMethods,
+                        ];
+
+                        $categoryNames = [];
+                        $categoryIds = $product->getCategoryIds();
+
+                        foreach ($categoryIds as $categoryId) {
+                            $category = $this->categoryRepository->get($categoryId);
+                            $categoryNames[] = $category->getName();
+                        }
+
+                        $productData = [
+                            "sku" => $product->getSku(),
+                            "url" => $product->getUrlKey(),
+                            'manufacturer' => $product->getAttributeText('country_of_manufacture'),
+                            "model" => $product->getModel(),
+                            "ean" => $product->getEan(),
+                            "price" => $product->getPrice(),
+                            'availability' => $product->isSalable() ? 'InStock' : 'OutOfStock',
+                            'itemsAvailable' => $product->getQty(),
+                            "itemCondition" => "NewCondition",
+                            "category" => $categoryNames,
+                            "name" => $product->getName(),
+                            "description" => $product->getDescription(),
+                            'updated' => $product->getUpdatedAt(),
+                            'delivery' => $deliveryOptions,
+                            'images' => $images
+                        ];
+
+                        $productsData[] = $productData;
+                    }
+                }
+            }
 
         } else {
             return ['error' => 'Incorrect Method!'];
