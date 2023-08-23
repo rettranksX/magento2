274daa01c5a67d569e46dfd9371d052d63f513b9
rettranksX <rettranks@gmail.com>
2023-08-23 15:34:14 +0300

    test

--- app/code/Dev/RestApi/Model/Api/ProductRepository.php
+++ app/code/Dev/RestApi/Model/Api/ProductRepository.php
@@ -195,7 +195,7 @@ class ProductRepository implements ProductRepositoryInterface
     public function getProducts(int $details): string
     {
 
-        $actualToken = 'evtch0nzcqshled4zg1lg99tiln59crk';
+        $actualToken = '8db80264ec5dec920a66562d774b509c';
 
         $authorizationHeader = $_SERVER['HTTP_AUTHORIZATION'];
 
