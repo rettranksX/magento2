e248d6494ccafeaf64ab97c7b51e2841e88b9551
rettranksX <rettranks@gmail.com>
2023-08-28 16:04:19 +0300

    Test Change old module

--- app/code/Dev/RestApi/Api/ProductRepositoryInterface.php
+++ app/code/Dev/RestApi/Api/ProductRepositoryInterface.php
@@ -3,21 +3,21 @@ namespace Dev\RestApi\Api;
 
 interface ProductRepositoryInterface
 {
-    /**
-     * Return a filtered product.
-     *
-     * @param int $id
-     * @return \Dev\RestApi\Api\ResponseItemInterface
-     * @throws \Magento\Framework\Exception\NoSuchEntityException
-     */
-    public function getItem(int $id);
-    /**
-     * Set descriptions for the products.
-     *
-     * @param \Dev\RestApi\Api\RequestItemInterface[] $products
-     * @return void
-     */
-    public function setDescription(array $products);
+    // /**
+    //  * Return a filtered product.
+    //  *
+    //  * @param int $id
+    //  * @return \Dev\RestApi\Api\ResponseItemInterface
+    //  * @throws \Magento\Framework\Exception\NoSuchEntityException
+    //  */
+    // public function getItem(int $id);
+    // /**
+    //  * Set descriptions for the products.
+    //  *
+    //  * @param \Dev\RestApi\Api\RequestItemInterface[] $products
+    //  * @return void
+    //  */
+    // public function setDescription(array $products);
 
 
     /**
--- app/code/Dev/RestApi/Model/Api/ProductRepository.php
+++ app/code/Dev/RestApi/Model/Api/ProductRepository.php
@@ -119,87 +119,87 @@ class ProductRepository implements ProductRepositoryInterface
         $this->jsonResultFactory = $jsonResultFactory;
     }
 
-    /**
-     * {@inheritDoc}
-     *
-     * @param int $id
-     * @return ResponseItemInterface
-     * @throws NoSuchEntityException
-     */
-    public function getItem(int $id): mixed
-    {
-        $collection = $this->getProductCollection()
-            ->addAttributeToFilter('entity_id', ['eq' => $id]);
-        /** @var ProductInterface $product */
-        $product = $collection->getFirstItem();
-        if (!$product->getId()) {
-            throw new NoSuchEntityException(__('Product not found'));
-        }
-        return $this->getResponseItemFromProduct($product);
-    }
-    /**
-     * {@inheritDoc}
-     *
-     * @param RequestItemInterface[] $products
-     * @return void
-     */
-    public function setDescription(array $products): void
-    {
-        foreach ($products as $product) {
-            $this->setDescriptionForProduct(
-                $product->getId(),
-                $product->getDescription()
-            );
-        }
-    }
+    // /**
+    //  * {@inheritDoc}
+    //  *
+    //  * @param int $id
+    //  * @return ResponseItemInterface
+    //  * @throws NoSuchEntityException
+    //  */
+    // public function getItem(int $id): mixed
+    // {
+    //     $collection = $this->getProductCollection()
+    //         ->addAttributeToFilter('entity_id', ['eq' => $id]);
+    //     /** @var ProductInterface $product */
+    //     $product = $collection->getFirstItem();
+    //     if (!$product->getId()) {
+    //         throw new NoSuchEntityException(__('Product not found'));
+    //     }
+    //     return $this->getResponseItemFromProduct($product);
+    // }
+    // /**
+    //  * {@inheritDoc}
+    //  *
+    //  * @param RequestItemInterface[] $products
+    //  * @return void
+    //  */
+    // public function setDescription(array $products): void
+    // {
+    //     foreach ($products as $product) {
+    //         $this->setDescriptionForProduct(
+    //             $product->getId(),
+    //             $product->getDescription()
+    //         );
+    //     }
+    // }
     /**
      * @return Collection
      */
-    private function getProductCollection(): mixed
-    {
-        /** @var Collection $collection */
-        $collection = $this->productCollectionFactory->create();
-        $collection
-            ->addAttributeToSelect(
-                [
-                    'entity_id',
-                    ProductInterface::SKU,
-                    ProductInterface::NAME,
-                    'description'
-                ],
-                'left'
-            );
-        return $collection;
-    }
+    // private function getProductCollection(): mixed
+    // {
+    //     /** @var Collection $collection */
+    //     $collection = $this->productCollectionFactory->create();
+    //     $collection
+    //         ->addAttributeToSelect(
+    //             [
+    //                 'entity_id',
+    //                 ProductInterface::SKU,
+    //                 ProductInterface::NAME,
+    //                 'description'
+    //             ],
+    //             'left'
+    //         );
+    //     return $collection;
+    // }
     /**
-     * @param ProductInterface $product
-     * @return ResponseItemInterface
-     */
-    private function getResponseItemFromProduct(ProductInterface $product): mixed
-    {
-        /** @var ResponseItemInterface $responseItem */
-        $responseItem = $this->responseItemFactory->create();
-        $responseItem->setId($product->getId())
-            ->setSku($product->getSku())
-            ->setName($product->getName())
-            ->setDescription($product->getDescription() ?? '');
-        return $responseItem;
-    }
-    /**
-     * Set the description for the product.
-     *
-     * @param int $id
-     * @param string $description
-     * @return void
-     */
-    private function setDescriptionForProduct(int $id, string $description): void
-    {
-        $this->productAction->updateAttributes(
-            [$id],
-            ['description' => $description],
-            $this->storeManager->getStore()->getId()
-        );
-    }
+    //  * @param ProductInterface $product
+    //  * @return ResponseItemInterface
+    //  */
+    // private function getResponseItemFromProduct(ProductInterface $product): mixed
+    // {
+    //     /** @var ResponseItemInterface $responseItem */
+    //     $responseItem = $this->responseItemFactory->create();
+    //     $responseItem->setId($product->getId())
+    //         ->setSku($product->getSku())
+    //         ->setName($product->getName())
+    //         ->setDescription($product->getDescription() ?? '');
+    //     return $responseItem;
+    // }
+    // /**
+    //  * Set the description for the product.
+    //  *
+    //  * @param int $id
+    //  * @param string $description
+    //  * @return void
+    //  */
+    // private function setDescriptionForProduct(int $id, string $description): void
+    // {
+    //     $this->productAction->updateAttributes(
+    //         [$id],
+    //         ['description' => $description],
+    //         $this->storeManager->getStore()->getId()
+    //     );
+    // }
     /**
      * {@inheritDoc}
      * @param int $details
