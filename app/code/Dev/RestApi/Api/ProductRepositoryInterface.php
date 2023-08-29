bbcd3db01808f4d82329f4f7cf4da7ea79f5b636
rettranksX <rettranks@gmail.com>
2023-08-29 10:08:23 +0300

    Delete old test modules

--- app/code/Dev/RestApi/Api/ProductRepositoryInterface.php
+++ app/code/Dev/RestApi/Api/ProductRepositoryInterface.php
@@ -3,23 +3,6 @@ namespace Dev\RestApi\Api;
 
 interface ProductRepositoryInterface
 {
-    // /**
-    //  * Return a filtered product.
-    //  *
-    //  * @param int $id
-    //  * @return \Dev\RestApi\Api\ResponseItemInterface
-    //  * @throws \Magento\Framework\Exception\NoSuchEntityException
-    //  */
-    // public function getItem(int $id);
-    // /**
-    //  * Set descriptions for the products.
-    //  *
-    //  * @param \Dev\RestApi\Api\RequestItemInterface[] $products
-    //  * @return void
-    //  */
-    // public function setDescription(array $products);
-
-
     /**
      * Get a list of products.
      *
@@ -27,14 +10,5 @@ interface ProductRepositoryInterface
      * @return string
      */
     public function getProducts(int $details): string;
-
-
-    // /**
-    //  * Get products by SKU.
-    //  *
-    //  * @param int $details
-    //  * @return array
-    //  */
-    // public function getProductsBySku(int $details): array;
 
 }
