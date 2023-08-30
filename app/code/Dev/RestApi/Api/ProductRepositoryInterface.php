b4b7d6c33c607b7b6b78a25dc2b94e830045e0ad
rettranksX <rettranks@gmail.com>
2023-08-30 16:16:02 +0300

    Change function execute()

--- app/code/Dev/RestApi/Api/ProductRepositoryInterface.php
+++ app/code/Dev/RestApi/Api/ProductRepositoryInterface.php
@@ -3,12 +3,12 @@ namespace Dev\RestApi\Api;
 
 interface ProductRepositoryInterface
 {
-    /**
-     * Get a list of products.
-     *
-     * @param int $details
-     * @return Json
-     */
-    public function getProducts(int $details);
+    // /**
+    //  * Get a list of products.
+    //  *
+    //  * @param int $details
+    //  * @return Json
+    //  */
+    public function execute();
 
 }
