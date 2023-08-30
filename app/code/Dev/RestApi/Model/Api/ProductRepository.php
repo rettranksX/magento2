e41c734e60faeba758562158cbbcb70dc7e48aba
rettranksX <rettranks@gmail.com>
2023-08-30 15:40:33 +0300

    Correct output changes

--- app/code/Dev/RestApi/Model/Api/ProductRepository.php
+++ app/code/Dev/RestApi/Model/Api/ProductRepository.php
@@ -257,7 +257,7 @@ class ProductRepository implements ProductRepositoryInterface
             $data['product'] = $data;
             $json_data = json_encode($data['product'], JSON_PRETTY_PRINT);
             
-            print($json_data);
+            return $json_data;
 
             // $jsonResponse = json_encode($response, JSON_PRETTY_PRINT);
             // print($jsonResponse);
