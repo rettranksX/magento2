f39830c239fa6d481bd773c24bd64cbf37031a71
rettranksX <rettranks@gmail.com>
2023-09-05 16:06:32 +0300

    change index.php

--- app/code/Dev/Controller/Adminhtml/Controller/Index.php
+++ app/code/Dev/Controller/Adminhtml/Controller/Index.php
@@ -5,7 +5,7 @@ use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
 use Magento\Framework\View\Result\PageFactory;
 
-class index extends Action
+class Index extends Action
 {
     protected $resultPageFactory;
 
