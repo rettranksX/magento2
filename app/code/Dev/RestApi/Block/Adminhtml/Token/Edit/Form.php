5ef34ba9813ee697edb64f50055e8463dd50c784
rettranksX <rettranks@gmail.com>
2023-09-12 20:40:11 +0300

    Add token

--- app/code/Dev/RestApi/Block/Adminhtml/Token/Edit/Form.php
+++ app/code/Dev/RestApi/Block/Adminhtml/Token/Edit/Form.php
@@ -12,7 +12,7 @@ class Form extends Generic
             [
                 'data' => [
                     'id' => 'edit_form',
-                    'action' => $this->getUrl('*/*/save'),
+                    'action' => $this->getUrl('*/*/Save'),
                     'method' => 'post',
                 ],
             ]
