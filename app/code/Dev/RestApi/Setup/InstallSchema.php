cbfa8252467055a87670f10b4e2e879a12817c5e
rettranksX <rettranks@gmail.com>
2023-09-12 12:32:51 +0300

    Add table

--- app/code/Dev/RestApi/Setup/InstallSchema.php
+++ app/code/Dev/RestApi/Setup/InstallSchema.php
@@ -4,54 +4,39 @@ namespace Dev\RestApi\Setup;
 use Magento\Framework\Setup\InstallSchemaInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
-use Magento\Framework\DB\Ddl\Table;
 
 class InstallSchema implements InstallSchemaInterface
 {
-    /**
-     * Installs DB schema for a module
-     *
-     * @param SchemaSetupInterface $setup
-     * @param ModuleContextInterface $context
-     * @return void
-     */
     public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
         $installer = $setup;
-
         $installer->startSetup();
 
         $table = $installer->getConnection()
-            ->newTable($installer->getTable('test_table'))
+            ->newTable($installer->getTable('token_table'))
             ->addColumn(
-                'entity_id',
-                Table::TYPE_INTEGER,
+                'token_id',
+                \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
                 null,
-                ['identity' => true, 'nullable' => false, 'primary' => true],
-                'ID'
+                ['identity' => true, 'unsigned' => true, 'nullable' => false, 'primary' => true],
+                'Token ID'
             )
             ->addColumn(
-                'user_name',
-                Table::TYPE_TEXT,
+                'token_value',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                255,
+                ['nullable' => false],
+                'Token Value'
+            )
+            ->addColumn(
+                'created_at',
+                \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
                 null,
-                ['nullable' => false, 'default' => ''],
-                'User Name'
+                ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
+                'Created At'
             )
-            ->addColumn(
-                'email',
-                Table::TYPE_TEXT,
-                null,
-                ['nullable' => false, 'default' => ''],
-                'User Email'
-            )
-            ->addColumn(
-                'value',
-                Table::TYPE_DECIMAL,
-                '12,4',
-                [],
-                'Value'
-            )
-            ->setComment('About Your Table');
+            ->setComment('Token Table');
+
         $installer->getConnection()->createTable($table);
 
         $installer->endSetup();
