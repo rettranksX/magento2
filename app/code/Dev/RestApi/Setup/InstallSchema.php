e62c4d73855893934dfb650b05efecf06ddb328b
rettranksX <rettranks@gmail.com>
2023-09-12 12:11:33 +0300

    Add table

--- app/code/Dev/RestApi/Setup/InstallSchema.php
+++ app/code/Dev/RestApi/Setup/InstallSchema.php
@@ -1,79 +1,59 @@
 <?php
 namespace Dev\RestApi\Setup;
+
 use Magento\Framework\Setup\InstallSchemaInterface;
 use Magento\Framework\Setup\ModuleContextInterface;
 use Magento\Framework\Setup\SchemaSetupInterface;
 use Magento\Framework\DB\Ddl\Table;
+
 class InstallSchema implements InstallSchemaInterface
 {
+    /**
+     * Installs DB schema for a module
+     *
+     * @param SchemaSetupInterface $setup
+     * @param ModuleContextInterface $context
+     * @return void
+     */
     public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
-        $installer = $setup; $installer->startSetup();
-        $tableName = $installer->getTable('test_helloworld');
-        if ($installer->getConnection()->isTableExists($tableName) != true)
-        {
-            $table = $installer->getConnection()
-                ->newTable($tableName)
-                ->addColumn(
-                    'id',
-                    Table::TYPE_INTEGER,
-                    null,
-                    [
+        $installer = $setup;
 
-                        'identity' => true,
-                        'unsigned' => true,
-                        'nullable' => false,
-                        'primary' => true
-                    ],
-                    'ID'
-                )
-                ->addColumn(
-                    'name',
-                    Table::TYPE_TEXT,
-                    null,
-                    [
-                        'nullable' => false,
-                        'default' => ''
-                    ],
-                    'Name'
-                )
-                ->addColumn(
-                    'description',
-                    Table::TYPE_TEXT,
-                    null,
-                    [
-                        'nullable' => false,
-                        'default' => ''
-                    ],
-                    'Description'
-                )
-                ->addColumn(
-                   'created_at',
-                    Table::TYPE_DATETIME,
-                    null,
-                    [
-                        'nullable' => false
+        $installer->startSetup();
 
-                    ],
+        $table = $installer->getConnection()
+            ->newTable($installer->getTable('test_table'))
+            ->addColumn(
+                'entity_id',
+                Table::TYPE_INTEGER,
+                null,
+                ['identity' => true, 'nullable' => false, 'primary' => true],
+                'ID'
+            )
+            ->addColumn(
+                'user_name',
+                Table::TYPE_TEXT,
+                null,
+                ['nullable' => false, 'default' => ''],
+                'User Name'
+            )
+            ->addColumn(
+                'email',
+                Table::TYPE_TEXT,
+                null,
+                ['nullable' => false, 'default' => ''],
+                'User Email'
+            )
+            ->addColumn(
+                'value',
+                Table::TYPE_DECIMAL,
+                '12,4',
+                [],
+                'Value'
+            )
+            ->setComment('About Your Table');
+        $installer->getConnection()->createTable($table);
 
-                    'Created At'
-                )
-                ->addColumn(
-                    'status',
-                    Table::TYPE_SMALLINT,
-                    null,
-                    [
-                        'nullable' => false,
-                        'default' => '0'
-                    ],
-                    'Status'
-
-                )
-                ->setComment('Test Table')
-                ->setOption('type', 'MariaDB')
-                ->setOption('charset', 'utf8');
-                $installer->getConnection()->createTable($table);
-        }
         $installer->endSetup();
     }
 }
