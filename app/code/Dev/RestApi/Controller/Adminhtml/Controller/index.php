79e6441c48b570bdd4ab9f1666f489c021b46a2a
rettranksX <rettranks@gmail.com>
2023-09-13 14:07:36 +0300

    Add token

--- app/code/Dev/RestApi/Controller/Adminhtml/Controller/index.php
+++ app/code/Dev/RestApi/Controller/Adminhtml/Controller/index.php
@@ -1,51 +1,51 @@
 <?php
 
-namespace Dev\RestApi\Controller\Adminhtml\Controller;
+// namespace Dev\RestApi\Controller\Adminhtml\Controller;
 
-use Magento\Backend\App\Action;
-use Magento\Backend\App\Action\Context;
-use Magento\Framework\App\Action\HttpGetActionInterface;
-use Magento\Framework\View\Result\Page;
-use Magento\Framework\View\Result\PageFactory;
+// use Magento\Backend\App\Action;
+// use Magento\Backend\App\Action\Context;
+// use Magento\Framework\App\Action\HttpGetActionInterface;
+// use Magento\Framework\View\Result\Page;
+// use Magento\Framework\View\Result\PageFactory;
 
-/**
- * Class Index
- */
-class Index extends Action implements HttpGetActionInterface
-{
-    const MENU_ID = 'Dev_RestApi::PriceInfo_Module_form';
+// /**
+//  * Class Index
+//  */
+// class Index extends Action implements HttpGetActionInterface
+// {
+//     const MENU_ID = 'Dev_RestApi::PriceInfo_Module_form';
 
-    /**
-     * @var PageFactory
-     */
-    protected $resultPageFactory;
+//     /**
+//      * @var PageFactory
+//      */
+//     protected $resultPageFactory;
 
-    /**
-     * Index constructor.
-     *
-     * @param Context $context
-     * @param PageFactory $resultPageFactory
-     */
-    public function __construct(
-        Context $context,
-        PageFactory $resultPageFactory
-    ) {
-        parent::__construct($context);
+//     /**
+//      * Index constructor.
+//      *
+//      * @param Context $context
+//      * @param PageFactory $resultPageFactory
+//      */
+//     public function __construct(
+//         Context $context,
+//         PageFactory $resultPageFactory
+//     ) {
+//         parent::__construct($context);
 
-        $this->resultPageFactory = $resultPageFactory;
-    }
+//         $this->resultPageFactory = $resultPageFactory;
+//     }
 
-    /**
-     * Load the page defined in view/adminhtml/layout/exampleadminnewpage_helloworld_index.xml
-     *
-     * @return Page
-     */
-    public function execute()
-    {
-        $resultPage = $this->resultPageFactory->create();
-        $resultPage->setActiveMenu(static::MENU_ID);
-        $resultPage->getConfig()->getTitle()->prepend(__('PriceInfo'));
+//     /**
+//      * Load the page defined in view/adminhtml/layout/exampleadminnewpage_helloworld_index.xml
+//      *
+//      * @return Page
+//      */
+//     public function execute()
+//     {
+//         $resultPage = $this->resultPageFactory->create();
+//         $resultPage->setActiveMenu(static::MENU_ID);
+//         $resultPage->getConfig()->getTitle()->prepend(__('PriceInfo'));
 
-        return $resultPage;
-    }
-}
+//         return $resultPage;
+//     }
+// }
