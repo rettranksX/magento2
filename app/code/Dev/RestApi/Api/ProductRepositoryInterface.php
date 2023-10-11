1f72cc17f1a152642e347391ae151bba85ecb03f
rettranksX <rettranks@gmail.com>
2023-10-11 17:02:31 +0300

    Create Controller, Model, View

--- app/code/Dev/RestApi/Api/ProductRepositoryInterface.php
+++ app/code/Dev/RestApi/Api/ProductRepositoryInterface.php
@@ -3,10 +3,16 @@ namespace Dev\RestApi\Api;
 
 interface ProductRepositoryInterface
 {
-    /**
-     * Get a list of products.
-     * @return string
-     */
-    public function execute();
+    // /**
+    //  * Get a list of products.
+    //  * @return string
+    //  */
+    // public function execute();
+
+    public function getProducts($details, $offset, $count);
+
+    public function getProductsBySku($details, $skuArray, $offset, $count);
+
+    public function getCountryCodeByFullName($countryName);
 
 }
