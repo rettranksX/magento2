0bf3b0039aa8cab9a36eec60102d62538c894c3f
rettranksX <rettranks@gmail.com>
2023-10-18 10:16:55 +0300

    Add test endpoint

--- app/code/Dev/RestApi/Controller/Index/Index.php
+++ app/code/Dev/RestApi/Controller/Index/Index.php
@@ -1,31 +1,31 @@
 <?php
-namespace Dev\RestApi\Controller\Index;
+// namespace Dev\RestApi\Controller\Index;
 
-use Magento\Framework\App\Action\Action;
-use Magento\Framework\App\Action\Context;
-use Magento\Framework\Controller\Result\JsonFactory;
+// use Magento\Framework\App\Action\Action;
+// use Magento\Framework\App\Action\Context;
+// use Magento\Framework\Controller\Result\JsonFactory;
 
-class Index extends Action
-{
-    protected $jsonResultFactory;
+// class Index extends Action
+// {
+//     protected $jsonResultFactory;
 
-    public function __construct(Context $context, JsonFactory $jsonResultFactory)
-    {
-        parent::__construct($context);
-        $this->jsonResultFactory = $jsonResultFactory;
-    }
+//     public function __construct(Context $context, JsonFactory $jsonResultFactory)
+//     {
+//         parent::__construct($context);
+//         $this->jsonResultFactory = $jsonResultFactory;
+//     }
 
-    public function execute()
-    {
-        // $data = ['message' => 'Hello, Magento API!'];
-        // $data = [
-        //     'name' => 'John',
-        //     'age' => 30,
-        //     'car' => null
-        // ];
-        $result = $this->jsonResultFactory->create();
-        $result->setData($data);
+//     public function execute()
+//     {
+//         // $data = ['message' => 'Hello, Magento API!'];
+//         // $data = [
+//         //     'name' => 'John',
+//         //     'age' => 30,
+//         //     'car' => null
+//         // ];
+//         $result = $this->jsonResultFactory->create();
+//         $result->setData($data);
 
-        return $result;
-    }
-}
+//         return $result;
+//     }
+// }
