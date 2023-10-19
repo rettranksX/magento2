73f75ff258002218abd69072e5a122a054e38ad6
rettranksX <rettranks@gmail.com>
2023-10-19 14:03:55 +0300

    Add test endpoint

--- app/code/Dev/RestApi/Model/TestModel.php
+++ app/code/Dev/RestApi/Model/TestModel.php
@@ -4,20 +4,17 @@ namespace Dev\RestApi\Model;
 use Dev\RestApi\Api\TestInterface;
 use Magento\Framework\Controller\Result\JsonFactory;
 
-
 class TestModel implements TestInterface
 {
-    protected JsonFactory $jsonFactory;
+    protected $jsonFactory;
 
-    public function __construct(
-        JsonFactory $jsonFactory
-    ){
+    public function __construct(JsonFactory $jsonFactory)
+    {
         $this->jsonFactory = $jsonFactory;
     }
 
     public function testMethod()
     {
-
         $data = [
             'name' => 'John',
             'age' => 30,
