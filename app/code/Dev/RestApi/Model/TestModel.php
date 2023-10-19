7b41dce90bd1fb17b2b89d8350b4314d842e524e
rettranksX <rettranks@gmail.com>
2023-10-19 14:21:01 +0300

    Add test endpoint

--- app/code/Dev/RestApi/Model/TestModel.php
+++ app/code/Dev/RestApi/Model/TestModel.php
@@ -18,9 +18,7 @@ class TestModel implements TestInterface
         $data = [
             'name' => 'John',
             'age' => 30,
-            'eeeeboyyy' => 123,
             'car' => null,
-            'some' => null,
             'address' => [
                 'street' => '123 Main St',
                 'city' => 'Anytown',
