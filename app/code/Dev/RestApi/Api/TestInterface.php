475a4d04b34b6677a6c400bfbde5fa9ae6ef2110
rettranksX <rettranks@gmail.com>
2023-10-23 14:04:36 +0300

    NEW TEST OBJECT FOR JSON DATA

--- app/code/Dev/RestApi/Api/TestInterface.php
+++ app/code/Dev/RestApi/Api/TestInterface.php
@@ -3,14 +3,8 @@ namespace Dev\RestApi\Api;
 
 use Dev\RestApi\Api\Data\MainDataInterface;
 
-
 interface TestInterface
 {
-    // /**
-    // * @return \Magento\Framework\Controller\Result\Json
-    // */
-    // public function testMethod();
-
     /**
      * @return MainDataInterface
      */
