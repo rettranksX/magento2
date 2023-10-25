8dcff034c85f471c34f3bea297acc5be4fe50198
rettranksX <rettranks@gmail.com>
2023-10-25 16:00:39 +0300

    Revert "NEW TEST OBJECT FOR JSON DATA"
    This reverts commit 913dca826eceac8f082ef5db909b68c7c4df1b28.

--- app/code/Dev/RestApi/Api/Data/ProductCollectionInterface.php
+++ app/code/Dev/RestApi/Api/Data/ProductCollectionInterface.php
@@ -4,18 +4,7 @@ namespace Dev\RestApi\Api\Data;
 
 interface ProductCollectionInterface
 {
-    /**
-     * Add a product to the collection.
-     *
-     * @param ProductInterface $product
-     * @return $this
-     */
     public function addProduct(ProductInterface $product);
 
-    /**
-     * Get the products in the collection.
-     *
-     * @return ProductInterface[]
-     */
     public function getProducts(): array;
 }
