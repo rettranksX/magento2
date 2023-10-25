6d05430e8fc0ac956fb9173e22ce43d24ef1d9af
rettranksX <rettranks@gmail.com>
2023-10-25 09:14:08 +0300

    NEW TEST OBJECT FOR JSON DATA

--- app/code/Dev/RestApi/Api/Data/ProductInterface.php
+++ app/code/Dev/RestApi/Api/Data/ProductInterface.php
@@ -4,7 +4,24 @@ namespace Dev\RestApi\Api\Data;
 
 interface ProductInterface
 {
+/**
+     * Get SKU of the product.
+     *
+     * @return string|null
+     */
     public function getSku(): ?string;
+
+    /**
+     * Get URL of the product.
+     *
+     * @return string|null
+     */
     public function getUrl(): ?string;
+
+    /**
+     * Get manufacturer of the product.
+     *
+     * @return string|null
+     */
     public function getManufacturer(): ?string;
 }
